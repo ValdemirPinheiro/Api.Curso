441497f209a2baef628eb2b637231ce940165597
Valdemir Pinheiro <pinheiroone@gmail.com>
2021-01-20 15:48:25 -0300

    Httpclient integration

--- web.mvc/Controllers/UsuarioController.cs
+++ web.mvc/Controllers/UsuarioController.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Text;
 using web.mvc.Models.Usuarios;
 
 namespace web.mvc.Controllers
@@ -13,6 +17,27 @@ namespace web.mvc.Controllers
         [HttpPost]
         public IActionResult Cadastrar(RegistrarUsuarioViewModelInput registrarUsuarioViewModelInput)
         {
+            var clientHandler = new HttpClientHandler();
+            clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
+
+
+            var httpClient = new HttpClient(clientHandler);
+            httpClient.BaseAddress = new Uri("https://localhost:5001/");
+
+           var registrarUsuarioViewModelInputJson = JsonConvert.SerializeObject(registrarUsuarioViewModelInput);
+            var httpContent = new StringContent(registrarUsuarioViewModelInputJson, Encoding.UTF8, "application/json");
+
+            var httpPost = httpClient.PostAsync("/api/v1/user/register", httpContent).GetAwaiter().GetResult();
+            
+            if(httpPost.StatusCode == System.Net.HttpStatusCode.Created)
+            {
+                                ModelState.AddModelError("", "Os dados foram cadastrados com sucesso");
+            }
+            else
+            {
+                                ModelState.AddModelError("", "Erro ao cadastrar");
+
+            }
             return View();
         }
 
